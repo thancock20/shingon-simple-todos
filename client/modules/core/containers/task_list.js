e0b52f533c80bddff4253b7ac39c81c9bd246ad9
Todd Hancock <thancock20@hotmail.com>
2016-09-12 15:05:03 -0500

    Collections

--- client/modules/core/containers/task_list.js
+++ client/modules/core/containers/task_list.js
@@ -4,11 +4,7 @@ import TaskList from '../components/task_list.jsx';
 
 export const composer = ({context}, onData) => {
   const {Meteor, Collections} = context();
-  const tasks = [
-    { _id: 1, text: 'This is task 1'},
-    { _id: 2, text: 'This is task 2'},
-    { _id: 3, text: 'This is task 3'}
-  ];
+  const tasks = Collections.Tasks.find().fetch();
   onData(null, {tasks});
 };
 
