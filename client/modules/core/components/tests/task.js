a24e0db4fc8e6cf614dbba0c4efa844d4792c533
Todd Hancock <thancock20@hotmail.com>
2016-09-13 14:38:20 -0500

    Refactor Task component test to use shallow instead of render

--- client/modules/core/components/tests/task.js
+++ client/modules/core/components/tests/task.js
@@ -1,26 +1,25 @@
 const {describe, it} = global;
 import {expect} from 'chai';
-import {render} from 'enzyme';
+import {shallow} from 'enzyme';
 import Task from '../task';
 
 describe('core.components.task', () => {
   it('should output li element with text inside', () => {
     const task = {text: 'Hello, World!'};
-    const el = render(<Task task={task} />);
+    const el = shallow(<Task task={task} />);
     expect(el.find('li').text()).to.be.match(/Hello\, World!/);
   });
 
   it('should have a delete button', () => {
     const task = {text: 'Hello, World!'};
-    const el = render(<Task task={task} />);
-    expect(el.find('button').length).to.equal(1);
-    expect(el.find('button')[0].attribs.class).to.contain('delete');
+    const el = shallow(<Task task={task} />);
+    expect(el.find('button').props().className).to.contain('delete');
   });
 
   it('should have a checkbox', () => {
     const task = {text: 'Hello, World!'};
-    const el = render(<Task task={task} />);
-    expect(el.find('input').length).to.equal(1);
-    expect(el.find('input')[0].attribs.type).to.equal('checkbox');
+    const el = shallow(<Task task={task} />);
+    expect(el.find('input').props().type).to.equal('checkbox');
+    expect(el.find('input').props().readOnly).to.equal(true);
   });
 });
