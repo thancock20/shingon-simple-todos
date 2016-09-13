aef2c7f32514dc4069cc3d589da9c496b7d7c61b
Todd Hancock <thancock20@hotmail.com>
2016-09-13 14:30:15 -0500

    Add test for NewTask component

--- client/modules/core/components/tests/new_task.js
+++ client/modules/core/components/tests/new_task.js
@@ -4,5 +4,12 @@ import {shallow} from 'enzyme';
 import NewTask from '../new_task';
 
 describe('core.components.new_task', () => {
-  it('should do something');
+  it('should render the text input', () => {
+    const el = shallow(<NewTask />);
+    const input = el.find('input');
+    expect(input.node.ref).to.equal('textInput');
+    expect(input.props().className).to.contain('newTask');
+    expect(input.props().type).to.equal('text');
+    expect(input.props().placeholder).to.equal('Type to add new tasks');
+  });
 });
