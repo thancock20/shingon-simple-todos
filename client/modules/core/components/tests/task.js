09299841ffbeb523303e49fad31f43bc8884ce58
Todd Hancock <thancock20@hotmail.com>
2016-09-13 13:47:30 -0500

    Add additional tests for Task component

--- client/modules/core/components/tests/task.js
+++ client/modules/core/components/tests/task.js
@@ -9,4 +9,18 @@ describe('core.components.task', () => {
     const el = render(<Task task={task} />);
     expect(el.find('li').text()).to.be.match(/Hello\, World!/);
   });
+
+  it('should have a delete button', () => {
+    const task = {text: 'Hello, World!'};
+    const el = render(<Task task={task} />);
+    expect(el.find('button').length).to.equal(1);
+    expect(el.find('button')[0].attribs.class).to.contain('delete');
+  });
+
+  it('should have a checkbox', () => {
+    const task = {text: 'Hello, World!'};
+    const el = render(<Task task={task} />);
+    expect(el.find('input').length).to.equal(1);
+    expect(el.find('input')[0].attribs.type).to.equal('checkbox');
+  });
 });
