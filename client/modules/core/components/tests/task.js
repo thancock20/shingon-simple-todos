c76053c7d2aab680b2e87ccc5c32c88e72ce2dda
Todd Hancock <thancock20@hotmail.com>
2016-09-15 12:52:08 -0500

    Get tests for task component to pass again

--- client/modules/core/components/tests/task.js
+++ client/modules/core/components/tests/task.js
@@ -3,22 +3,29 @@ import {expect} from 'chai';
 import {shallow} from 'enzyme';
 import Task from '../task';
 
+const render = (task, showPrivateButton = true) => {
+  return (shallow(<Task
+    task={task}
+    showPrivateButton={showPrivateButton}
+    toggleChecked={() => {}}
+    togglePrivate={() => {}}
+    deleteTask={() => {}} />)
+  );
+};
+
 describe('core.components.task', () => {
   it('should output li element with text inside', () => {
-    const task = {text: 'Hello, World!'};
-    const el = shallow(<Task task={task} />);
+    const el = render({text: 'Hello, World!'});
     expect(el.find('li').text()).to.be.match(/Hello\, World!/);
   });
 
   it('should have a delete button', () => {
-    const task = {text: 'Hello, World!'};
-    const el = shallow(<Task task={task} />);
+    const el = render({text: 'Hello, World!'}, false);
     expect(el.find('button').props().className).to.contain('delete');
   });
 
   it('should have a checkbox', () => {
-    const task = {text: 'Hello, World!'};
-    const el = shallow(<Task task={task} />);
+    const el = render({text: 'Hello, World!'});
     expect(el.find('input').props().type).to.equal('checkbox');
     expect(el.find('input').props().readOnly).to.equal(true);
   });
