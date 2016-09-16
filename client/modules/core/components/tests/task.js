fac63baa20fe925d38e722666c3f9c880d4f8999
Todd Hancock <thancock20@hotmail.com>
2016-09-16 12:49:11 -0500

    Add test for private button to task component

--- client/modules/core/components/tests/task.js
+++ client/modules/core/components/tests/task.js
@@ -24,11 +24,17 @@ describe('core.components.task', () => {
     expect(el.find('button').nodes[0].props.className).to.contain('delete');
   });
 
-  it('should have a public button', () => {
+  it('should have a public button normally', () => {
     const el = render({text: 'Hello, World!'});
     expect(el.find('button').nodes[1].props.className).to.contain('togglePrivate');
     expect(el.find('button').nodes[1].props.children).to.equal('Public');
-  })
+  });
+
+  it('should have a private button when private is true', () => {
+    const el = render({text: 'Hello, World!', private: true});
+    expect(el.find('button').nodes[1].props.className).to.contain('togglePrivate');
+    expect(el.find('button').nodes[1].props.children).to.equal('Private');
+  });
 
   it('should have a checkbox', () => {
     const el = render({text: 'Hello, World!'});
