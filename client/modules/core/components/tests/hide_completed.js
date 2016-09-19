f0969c03d8c5d81e28e02202a42d0571a3f57681
Todd Hancock <thancock20@hotmail.com>
2016-09-19 01:19:32 -0500

    Add interaction test for hide completed component

--- client/modules/core/components/tests/hide_completed.js
+++ client/modules/core/components/tests/hide_completed.js
@@ -1,10 +1,18 @@
 const {describe, it} = global;
 import {expect} from 'chai';
 import {shallow} from 'enzyme';
+import {spy} from 'sinon';
 import HideCompleted from '../hide_completed';
 
 describe('core.components.hide_completed', () => {
   it('should render without exploding', () => {
     expect(shallow(<HideCompleted />).length).to.equal(1);
+  });
+
+  it('should call toggleHideCompleted when checkbox clicked', () => {
+    const toggleHideCompleted = spy();
+    const el = shallow(<HideCompleted toggleHideCompleted={toggleHideCompleted} />);
+    el.find('input').simulate('click');
+    expect(toggleHideCompleted.called).to.equal(true);
   });
 });
