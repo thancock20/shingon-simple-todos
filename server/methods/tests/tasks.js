342508555f9e191c3fd2a652bc2470cb1985bfe4
Todd Hancock <thancock20@hotmail.com>
2016-09-20 16:04:13 -0500

    Add gagarin integration tests for methods

---
+++ server/methods/tests/tasks.js
@@ -0,0 +1,77 @@
+/*
+  global
+  describe it expect before
+  ddp meteor Accounts
+*/
+
+describe('tasks', () => {
+  const server = meteor({flavor: 'fiber'});
+  const client = ddp(server, {flavor: 'fiber'});
+
+  before(() => {
+    server.execute(() => {
+      Accounts.createUser({ username: 'Test', password: 'password'});
+    });
+
+    client.login({user: {username: 'Test' }, password: 'password' });
+    client.call('tasks.create', [ {text: 'Hello, World!'} ]);
+    client.sleep(200);
+
+    client.subscribe('tasks');
+  });
+
+  describe('tasks.create', () => {
+    it('should create a task', () => {
+      var tasks = client.collection('tasks');
+
+      const taskCount = Object.keys(tasks).length;
+
+      client.call('tasks.create', [ {text: 'Hello, again!'} ]);
+      client.sleep(200);
+      tasks = client.collection('tasks');
+
+      expect(Object.keys(tasks).length).to.equal(taskCount + 1);
+    });
+  });
+
+  describe('tasks.update', () => {
+    it('should change the checked field', () => {
+      var tasks = client.collection('tasks');
+      const id = Object.keys(tasks)[0];
+
+      expect(tasks[id].checked).to.equal(false);
+
+      client.call('tasks.update', [ id, {checked: true} ]);
+      client.sleep(200);
+      tasks = client.collection('tasks');
+
+      expect(tasks[id].checked).to.equal(true);
+    });
+
+    it('should change the private field', () => {
+      var tasks = client.collection('tasks');
+      const id = Object.keys(tasks)[0];
+
+      expect(tasks[id].private).to.equal(false);
+
+      client.call('tasks.update', [ id, {private: true} ]);
+      client.sleep(200);
+      tasks = client.collection('tasks');
+
+      expect(tasks[id].private).to.equal(true);
+    });
+  });
+
+  describe('tasks.delete', () => {
+    it('should delete a task', () => {
+      var tasks = client.collection('tasks');
+      const taskCount = Object.keys(tasks).length;
+      const id = Object.keys(tasks)[0];
+      client.call('tasks.delete', [ id ]);
+      client.sleep(200);
+      tasks = client.collection('tasks');
+
+      expect(Object.keys(tasks).length).to.equal(taskCount - 1);
+    });
+  });
+});
