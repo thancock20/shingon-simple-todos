b2daf32ae7fcb093f9e11bbcf07d6fe552dc8421
Todd Hancock <thancock20@hotmail.com>
2016-09-22 13:01:32 -0500

    Change method of specifying unpublished fields

--- server/publications/tasks.js
+++ server/publications/tasks.js
@@ -2,6 +2,16 @@ import {Tasks} from '/lib/collections';
 import {Meteor} from 'meteor/meteor';
 import {check} from 'meteor/check';
 
+const getUnpublished = () => {
+  const fields = {};
+  if (Tasks.unpublished) {
+    Tasks.unpublished.forEach((field) => {
+      fields[field] = 0;
+    });
+  }
+  return fields;
+};
+
 export default function () {
   Meteor.publish('tasks', function () {
     return Tasks.find({
@@ -9,7 +19,6 @@ export default function () {
         { private: { $ne: true } },
         { owner: this.userId }
       ]
-    }, Tasks.publish
-);
+    }, {fields: getUnpublished()});
   });
 }
