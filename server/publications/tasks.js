70bc47e5e8363c0e088c7ff82e3f9111892fdacd
Todd Hancock <thancock20@hotmail.com>
2016-09-26 16:15:20 -0500

    Extract getUnpublishedFields utility function to lib

--- server/publications/tasks.js
+++ server/publications/tasks.js
@@ -1,16 +1,7 @@
 import {Task} from '/lib/collections';
 import {Meteor} from 'meteor/meteor';
 import {check} from 'meteor/check';
-
-const getUnpublished = () => {
-  const fields = {};
-  if (Task.unpublished) {
-    Task.unpublished.forEach((field) => {
-      fields[field] = 0;
-    });
-  }
-  return fields;
-};
+import getUnpublishedFields from '/lib/getUnpublishedFields';
 
 export default function () {
   Meteor.publish('tasks', function () {
@@ -19,6 +10,6 @@ export default function () {
         { private: { $ne: true } },
         { owner: this.userId }
       ]
-    }, {fields: getUnpublished()});
+    }, getUnpublishedFields(Task));
   });
 }
