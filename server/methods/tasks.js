79ca54dbd1399ef10d2414fcb2f7d8e4ef3652d0
Todd Hancock <thancock20@hotmail.com>
2016-09-27 13:37:09 -0500

    Extract default methods boilerplate to utility method

--- server/methods/tasks.js
+++ server/methods/tasks.js
@@ -1,29 +1,6 @@
 import {Task} from '/lib/collections';
-import {Meteor} from 'meteor/meteor';
-import {check} from 'meteor/check';
+import defaultMethods from '/lib/default_methods';
 
 export default function () {
-  Meteor.methods({
-    'tasks.create'(taskToInsert) {
-      check(taskToInsert, Object);
-
-      const task = new Task();
-      task.create(taskToInsert);
-    },
-
-    'tasks.edit'(taskId, updates) {
-      check(taskId, String);
-      check(updates, Object);
-
-      const task = Task.findOne(taskId);
-      task.edit(updates);
-    },
-
-    'tasks.delete'(taskId) {
-      check(taskId, String);
-
-      const task = Task.findOne(taskId);
-      task.remove();
-    }
-  });
+  defaultMethods('tasks', Task);
 }
