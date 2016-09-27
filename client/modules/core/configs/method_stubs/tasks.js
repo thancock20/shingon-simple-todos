88eeb597c4853cc02c901a316cb73375a468582b
Todd Hancock <thancock20@hotmail.com>
2016-09-27 13:59:43 -0500

    Change method_stubs to use defaultMethods function

--- client/modules/core/configs/method_stubs/tasks.js
+++ client/modules/core/configs/method_stubs/tasks.js
@@ -1,29 +1,7 @@
-import {check} from 'meteor/check';
+import defaultMethods from '/lib/default_methods';
 
-export default function ({Meteor, Collections}) {
+export default function ({Collections}) {
   const {Task} = Collections;
 
-  Meteor.methods({
-    'tasks.create'(taskToInsert) {
-      check(taskToInsert, Object);
-
-      const task = new Task();
-      task.create(taskToInsert);
-    },
-
-    'tasks.edit'(taskId, updates) {
-      check(taskId, String);
-      check(updates, Object);
-
-      const task = Task.findOne(taskId);
-      task.edit(updates);
-    },
-
-    'tasks.delete'(taskId) {
-      check(taskId, String);
-
-      const task = Task.findOne(taskId);
-      task.remove();
-    }
-  });
+  defaultMethods('tasks', Task);
 }
