51e39545cf8b5af8516c33ffc9157cd913977125
Todd Hancock <thancock20@hotmail.com>
2016-10-13 12:53:03 -0500

    Add all of AltAccounts.config to file

--- client/modules/accounts/configs/alt-accounts-config.js
+++ client/modules/accounts/configs/alt-accounts-config.js
@@ -1,4 +1,28 @@
 export default () => {
-  AltAccounts.config.passwordSignupFields = 'USERNAME_ONLY';
-  AltAccounts.config.text.logInStatus = 'Sign In';
+  AltAccounts.config = {
+    passwordSignupFields: 'USERNAME_ONLY',
+    text: {
+      logInStatus: 'Log In',
+      logOutStatus: 'Log Out',
+
+      oauthPasswordSeparator: 'OR',
+
+      userNameField: 'User Name',
+      userNameOrEmailField: 'User Name / Email',
+      emailField: 'Email',
+      optionalEmailField: 'Email (Optional)',
+      passwordField: 'Password',
+      passwordField2: 'Confirm Password',
+
+      logInButton: 'Sign In',
+      createAccountButton: 'Create Account',
+      sendEmailButton: 'Send Email',
+      okButton: 'OK',
+
+      logInLink: 'Sign In',
+      createAccountLink: 'Create Account',
+      forgotPasswordLink: 'Forgot Password',
+      closeLink: 'Close'
+    }
+  };
 };
